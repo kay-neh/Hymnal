ee8e93a5bb02ecdfdf46722f5b7a5b278cf1713d
kay-neh <59393861+kay-neh@users.noreply.github.com>
2020-08-27 23:00:51 +0100

    Initialize repo

---
+++ app/src/main/java/com/example/hymnal/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.hymnal;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+
+import com.example.hymnal.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+
+        NavController controller = Navigation.findNavController(this,R.id.my_nav_host_fragment);
+        NavigationUI.setupWithNavController(binding.bottomNavBar, controller);
+    }
+}
